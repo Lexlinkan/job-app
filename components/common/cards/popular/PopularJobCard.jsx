81a6aaa46606fe053b92b22d690440e448e897c6
Lexlinkan <alexander-lindqvist@live.se>
2023-11-01 23:04:22 +0100

    second commit, halfway

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 
 import styles from './popularjobcard.style'
 
 const PopularJobCard = () => {
   return (
-    <View>
+    <TouchableOpacity>
       <Text>PopularJobCard</Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
