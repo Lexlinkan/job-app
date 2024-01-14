d4e456f36ff93bfeceb1fc915c0d933d71a2b048
Lexlinkan <alexander-lindqvist@live.se>
2024-01-14 20:34:08 +0100

    updated

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -22,8 +22,10 @@ const Popularjobs = () => {
 
   const [selectedJob, setSelectedJob] = useState()
   const handleCardPress = (item) => {
-    
-  }
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
 
   console.log(data);
 
@@ -47,6 +49,8 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard 
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
